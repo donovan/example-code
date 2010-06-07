f027b52c68ad9ecd927150efbe8eeb626f734eb5
Donovan Jones <donovan@catalyst.net.nz>
2010-06-08 10:45:01 +1200

    add different ways to get same result for future-date

--- perl/time/future-date.pl
+++ perl/time/future-date.pl
@@ -20,3 +20,13 @@ $month = 0 . $month if $month < 10;
 $year = $year +1900;
 
 print "In $days days time the date will be $year-$month-$mday\n";
+
+# less long winded way to do the same thing
+my ($d, $m, $y) = (localtime($unix_then))[3,4,5];
+my $date_then = sprintf('%04d-%02d-%02d', $y+1900, $m+1, $d);
+print "In $days days time the date will be $date_then\n";
+
+# if you have postgres avalible then the easiest way is to do:
+# my $due = $dbh->selectrow_array("SELECT (current_timestamp + '7 days'::interval)::date AS date_in_seven_days");
+print "if you have postgres available then you should be using:\n";
+print qq{my \$due = \$dbh->selectrow_array("SELECT (current_timestamp + '7 days'::interval)::date AS date_in_seven_days");\n};
