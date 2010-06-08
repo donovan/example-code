395b7b86562628c4765b5539d4b74acba805ae0d
Donovan Jones <donovan@catalyst.net.nz>
2010-06-08 16:38:44 +1200

    golf with time

--- perl/time/future-date.pl
+++ perl/time/future-date.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+use POSIX; # provides strftime
+
 my $days = shift;
 
 my $days_in_seconds = $days * 24 * 60 * 60;
@@ -12,6 +14,8 @@ my $unix_now = time;
 my $unix_then = $unix_now + $days_in_seconds;
 
 print "unix_then = $unix_then unix_now = $unix_now\n";
+
+print "\n";
 
 my ($mday, $month, $year) = (localtime($unix_then))[3,4,5];
 $mday = 0 . $mday if $mday < 10;
@@ -26,7 +30,10 @@ my ($d, $m, $y) = (localtime($unix_then))[3,4,5];
 my $date_then = sprintf('%04d-%02d-%02d', $y+1900, $m+1, $d);
 print "In $days days time the date will be $date_then\n";
 
-# if you have postgres avalible then the easiest way is to do:
-# my $due = $dbh->selectrow_array("SELECT (current_timestamp + '7 days'::interval)::date AS date_in_seven_days");
+# even less longwinded version using strftime
+print "In $days days time the date will be " . strftime('%F', localtime($unix_then)) . "\n";
+
+print "\n";
+
 print "if you have postgres available then you should be using:\n";
 print qq{my \$due = \$dbh->selectrow_array("SELECT (current_timestamp + '7 days'::interval)::date AS date_in_seven_days");\n};
