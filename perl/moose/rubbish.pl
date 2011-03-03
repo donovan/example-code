01b26ffcff319ff403507a4409a96683f74b0b01
Donovan Jones <donovan@catalyst.net.nz>
2011-03-04 11:58:41 +1300

    add debug output looking at the moose meta info

--- perl/moose/rubbish.pl
+++ perl/moose/rubbish.pl
@@ -6,6 +6,8 @@ use 5.010;
 
 use Trash;
 
+my $debug = 1;
+
 my @trash = (
     Trash->new( name => 'beer bottle',      type => 'glass',       size => 'small' ),
     Trash->new( name => 'cereal box',       type => 'cardboard',   size => 'small' ),
@@ -16,4 +18,25 @@ my @trash = (
 
 foreach my $piece_of_trash (@trash) {
     $piece_of_trash->show_vital_stats();
+}
+
+if ($debug) {
+
+    my $meta = Trash->meta();
+
+    print "\n--- ATTRIBUTES ---\n\n";
+    foreach my $attribute ( $meta->get_all_attributes ) {
+        print $attribute->name(), "\n";
+
+        if ( $attribute->has_type_constraint ) {
+            print "  type: ", $attribute->type_constraint->name, "\n";
+        }
+    }
+
+    print "\n--- METHODS ---\n\n";
+    for my $method ( sort { $a->package_name cmp $b->package_name || $a->name cmp $b->name } $meta->get_all_methods ) {
+        print $method->name, "\n";
+        print "    " . $method->package_name . "\n";
+    }
+
 }
