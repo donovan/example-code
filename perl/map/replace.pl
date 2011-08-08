091eeccd316b6acbd1d0aca2e0ca776b83988cfd
Donovan Jones <donovan@catalyst.net.nz>
2011-08-08 13:22:27 +1200

    assign to new array example

--- perl/map/replace.pl
+++ perl/map/replace.pl
@@ -12,8 +12,14 @@ my @items = qw(
     Bar-Baz
 );
 
+my @new = map { s{-}{ }g; $_ } @items;
+
 map { s{-}{ }g } @items;
 
 foreach my $item (@items) {
+    print "$item\n";
+}
+
+foreach my $item (@new) {
     print "$item\n";
 }
