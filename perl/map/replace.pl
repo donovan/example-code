47323d68a4eb6ff089654454377b573b71bebc72
Donovan Jones <donovan@catalyst.net.nz>
2012-03-21 10:18:21 +1300

    examples of modify in place

--- perl/map/replace.pl
+++ perl/map/replace.pl
@@ -12,14 +12,25 @@ my @items = qw(
     Bar-Baz
 );
 
+my @items2 = @items;
+my @items3 = @items;
+
 my @new = map { s{-}{ }g; $_ } @items;
 
-map { s{-}{ }g } @items;
+map { s{-}{ }g } @items2;
 
-foreach my $item (@items) {
+s{-}{ }g for @items3;
+
+print "-----------items------------\n";
+foreach my $item (@new) {
     print "$item\n";
 }
 
-foreach my $item (@new) {
+print "-----------items2------------\n";
+foreach my $item (@items2) {
+    print "$item\n";
+}
+print "-----------items3------------\n";
+foreach my $item (@items3) {
     print "$item\n";
 }
